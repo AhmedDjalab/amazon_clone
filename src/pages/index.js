c3a50d14482bb320d9909eeaa4b53f3a5e32ec99
AhmedDja <48105133+AhmedLight92@users.noreply.github.com>
2021-09-22 10:17:49 +0100

    Finishing the Banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100 h-screen">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
       {/* header  */}
       <Header />
-
+      <main className="max-w-screen-xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* Product Items */}
+      </main>
       {/* ---- ---- */}
     </div>
   );
