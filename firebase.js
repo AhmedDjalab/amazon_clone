ed1d9a975794b84a6692ad33b4bb47261ea56048
AhmedDja <48105133+AhmedLight92@users.noreply.github.com>
2021-09-23 09:34:18 +0100

    Adding Authentication

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSPm8Mhc41d_Ikp3vpOQFztZBj1QfDQHA",
+  authDomain: "clone-65fcc.firebaseapp.com",
+  projectId: "clone-65fcc",
+  storageBucket: "clone-65fcc.appspot.com",
+  messagingSenderId: "71712440257",
+  appId: "1:71712440257:web:03ccfd86db5cfdb67d3940",
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,19 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { signIn, signout, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 function Header() {
+  const [session] = useSession();
+  const router = useRouter();
   return (
     <header>
       {/* top  */}
       <div className="flex items-center bg-amazon_blue flex-grow py-3">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -38,15 +43,18 @@ function Header() {
           className="text-white flex items-center
          text-xs space-x-6 mx-6 whitespace-nowrap"
         >
-          <div className="link">
-            <p>Hello Ahmed </p>
+          <div onClick={!session ? signIn : signout} className="link">
+            <p>{session ? `Hello ${session.user.name}` : "Sign In"} </p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
           <div className="link">
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="relative link flex items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative link flex items-center"
+          >
             <span
               className="absolute top-0 right-0 
             md:right-10 h-4 w-4 bg-yellow-400 text-center
