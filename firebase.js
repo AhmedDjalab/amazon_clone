9efe763d716b4655f31d67b7f1ef425c2287098c
AhmedDja <48105133+AhmedLight92@users.noreply.github.com>
2021-09-24 12:51:30 +0100

    the orders page

--- firebase.js
+++ firebase.js
@@ -8,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "71712440257",
   appId: "1:71712440257:web:03ccfd86db5cfdb67d3940",
 };
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export default db;
