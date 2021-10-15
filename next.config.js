fc2804da8524d9f1db1fd67bc3318c47449d700c
AhmedDja <48105133+AhmedLight92@users.noreply.github.com>
2021-10-15 12:38:56 +0100

    fix env bug #6

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
- 
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  },
 };
